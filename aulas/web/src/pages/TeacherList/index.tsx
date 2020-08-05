e5ba853a8605bbc1b544b3f0dbaa33096320bd76
Bruno Juliao <brunojuliao.net@gmail.com>
2020-08-05 09:56:00 -0300

    Styling Teacher List; First component created

--- aulas/web/src/pages/TeacherList/index.tsx
+++ aulas/web/src/pages/TeacherList/index.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import { logoImg } from '../../assets/images/logo.svg';
-import { backIcon } from '../../assets/images/icons/back.svg';
 
 import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy" />
-                </div>
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject"/>
+                    </div>
 
-                <div className="header-content">
-                    <strong>Estes são os proffys disponíveis.</strong>
-                </div>
-            </header>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars3.githubusercontent.com/u/2057387?s=460&v=4" alt="Bruno Julião"/>
+                        <div>
+                            <strong>Bruno Julião</strong>
+                            <span>C#</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Testando a primeira linha da descrição 
+                        <br/><br/>
+                        Muito texto para alcançar as três linhas do layout base e assim poder validar se os ajustes aplicados no CSS estão adequados/corretos. Caso não esteja, favor corrigir.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 50,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="WhatsApp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
